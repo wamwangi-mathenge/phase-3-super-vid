4f9eda58855fe3b849f089759e9a92db3db33455
Brian <wamwangimathenge@gmail.com>
2023-02-16 13:23:51 +0300

    Calling the method of the same name defined in the User superclass

--- run.rb
+++ run.rb
@@ -1,6 +1,7 @@
 class User
 
     def log_in
+        puts "User#log_in called"
         @logged_in = true
     end
 end
@@ -8,7 +9,11 @@ end
 class Student < User
 
     def log_in
+        puts "Student#log_in called"
         super
         @in_class = true
     end
 end
+
+brian = Student.new
+brian.log_in
